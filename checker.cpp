3d8c708e9a5b056c3a4b657a8f64dcf681071970
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-03-02 22:53:07 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -12,7 +12,7 @@ bool isBatteryParameterInRange(float Parameter_value, float max, float min)
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
     return isBatteryParameterInRange(temperature,0,45) && 
     isBatteryParameterInRange(soc,20,80) &&
-    isBatteryParameterInRange(chargeRate, chargeRate -1 , 0.8)
+    isBatteryParameterInRange(chargeRate, chargeRate -1 , 0.8);
  // if(temperature < 0 || temperature > 45) {
  //   cout << "Temperature out of range!\n";
  //  return false;
